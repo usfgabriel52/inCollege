eaa273bf3b14dd0874a3d96197057f6c5814cdfe
gc12301 <gabe.r.cortez@live.com>
2021-09-19 21:23:28 -0400

    tests for Epic 1 Stories 4,5,6

---
+++ test_GabrielCortez.py
@@ -0,0 +1,32 @@
+import sys
+import io
+from typing import NoReturn
+import pytest
+
+from pytest_mock import mocker
+import main
+
+def test_Main_Menu():
+    sys.stdin = io.StringIO('0')
+    assert main.display_MainMenu() == None
+
+def test_findJob():
+    sys.stdin = io.StringIO('1 \n 0\n')
+    
+    
+    assert main.display_MainMenu() == None
+
+def test_findSomeone():
+    sys.stdin = io.StringIO('1 \n 0\n')
+    
+    
+    assert main.display_MainMenu() == None
+
+def test_skillsMenu():
+    sys.stdin = io.StringIO('3 \n 1\n2\n3\n4\n5\n0\n0\n')
+    
+    assert main.display_MainMenu() == None    
+
+
+
+
