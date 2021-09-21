101f258e09d28b799ca3be0b452a6da3e56f6257
zchenoweth <90414605+zchenoweth@users.noreply.github.com>
2021-09-21 19:28:43 -0400

    Add files via upload

---
+++ InCollege Code/menus.py
@@ -0,0 +1,143 @@
+from messages import *
+from verify_acc import *
+
+global logged_in
+
+
+def homeMenu():
+    
+    print("Welcome to InCollege.")
+    
+    while True:   
+        printHomeMenu()
+        select = input("Enter command: ")
+
+        if(select == "1" or select == "2" or select == "3" or select == "0"):
+            break
+        else:
+            printInvalidEntry()
+            continue
+
+    if(select == "1"):
+        loginMenu()
+    elif(select == "2"):
+        createAccountMenu()
+    elif(select == "3"):
+        studentSuccess()
+    elif(select == "0"):
+        print("Thank you for using InCollege!")
+        quit()
+
+
+
+
+
+def loginMenu():
+    
+    print("\nLog In to InCollege\n")
+
+    found = False
+
+    while not found:  # continue looping as long as user inputs invalid login info
+        # get input from the user
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+
+        # check if username and password are valid
+        found = find_account(username, password)
+        if not found:
+            print("Incorrect username/password, please try again\n")
+    print("\nYou have successfully logged in\n")
+    logged_in = True
+    mainMenu()
+
+
+
+
+
+def createAccountMenu():
+    
+    print("\nCreate a New Account\n")
+
+    # read CSV file containing all account information
+    accounts = pd.read_csv('accounts.csv', index_col=0, squeeze=True, header=None).to_dict()
+
+    # TODO: looping while user inputs invalid values
+    if len(accounts.keys()) < 5:
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+
+        # check if username is unique and password is valid
+        if not username in accounts and password_check(password):
+            accounts[username] = password  # add <username, password> pair into dict
+            print("Successfully created an account\n")
+    else:
+        print("All permitted accounts have been created, please come back later\n")
+
+    # save accounts dict into CSV file
+    df = pd.DataFrame(accounts.items())
+    df.to_csv('accounts.csv', index=False, header=False)
+    return 0
+
+
+
+
+
+def mainMenu():
+    
+    while True:
+        printMainMenu()
+    
+        opt = input("Enter command: ")
+
+        if int(opt) == 1: #search for job
+            printUnderConstruction()
+        elif int(opt) == 2:  
+            searchPeople()
+        elif int(opt) == 3:  # learn a new skill
+            skillMenu()
+        else:
+            logged_in = False
+            print("You have sucessfully logged out!\n")
+            break
+    return 0
+
+
+
+
+
+def skillMenu():
+    
+    while True:
+        printSkillList()
+
+        skill = input("Select a skill: ")
+    
+        if skill != "0":
+            printUnderConstruction()
+        else:
+            break
+    return 0
+
+
+
+
+def studentSuccess():
+    while True:
+        printStudentSuccess()
+    
+        select = input("Enter command: ")
+
+        if(select == "1"):
+            print("\nVideo is now playing...\n")
+        elif(select == "0"):
+            print("\nReturning to Home menu...\n")
+            break
+    return 0
+
+
+
+
+
+def searchPeople():
+    return
---
+++ InCollege Code/messages.py
@@ -0,0 +1,51 @@
+def printHomeMenu():
+    print("(1) Login\n"
+          "(2) Register\n"
+          "(3) Student Success Story\n"
+          "(0) Exit\n")
+    return 0
+
+
+
+def printInvalidEntry():
+    print("\nINVALID ENTRY PLEASE TRY AGAIN.\n")
+
+
+
+def printUnderConstruction():
+    print("\nUnder Construction...\n")
+    return 0
+
+
+def printMainMenu():
+    print("InCollege Main Menu\n"
+          "(1) Search for a job\n"
+          "(2) Find someone you know\n"
+          "(3) Learn a new skill\n"
+          "(0) Logout\n")
+    return 0
+
+
+def printSkillList():
+    
+    print("Skill list: ")  # tentative skill list
+    
+    print("(1) Python Programming\n"
+          "(2) Java Programming\n"
+          "(3) C++ Programming\n"
+          "(4) Time Management\n"
+          "(5) Effective communication\n"
+          "(0) Return to Main Menu\n")
+    return 0
+
+
+
+
+
+def printStudentSuccess():
+    
+    print("\nStudent Success Stories\n")
+    
+    print("(1) View Video\n"
+          "(0) Return to Home Menu")
+    return 0
