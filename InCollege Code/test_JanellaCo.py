6f9382bbad0aa1ce8b2f4d1af3572c42b40cc433
nella <29736011+janellamarie@users.noreply.github.com>
2021-10-03 19:05:50 -0400

    Added test for signing up
    Testing whether the program correctly sets the default values for newly created accounts

--- InCollege Code/test_JanellaCo.py
+++ InCollege Code/test_JanellaCo.py
@@ -7,7 +7,7 @@ import menus
 # testing option 6 - Important Links Menu when not logged in
 def test_impLinks(monkeypatch):
     # string that simulates input
-    input = '6\n1\n0\n2\n3\n4\n5\n' \
+    input = '1\n0\n2\n3\n4\n5\n' \
             '1\n2\n0\n' \
             '6\n7\n8\n' \
             '9\n10\n0\n'
@@ -16,27 +16,39 @@ def test_impLinks(monkeypatch):
     monkeypatch.setattr('sys.stdin', io.StringIO(input))
 
     # run the function we want to test and check output value
-    assert menus.homeMenu() == None
+    assert menus.impLinksMenu() == 0
 
 def test_usefulLinks(monkeypatch):
-    input='5\n2\n3\n4\n0\n0\n0'
+    input='2\n3\n4\n1\n0\n0\n'
 
     monkeypatch.setattr('sys.stdin', io.StringIO(input))
 
-    assert menus.homeMenu() == None
+    assert menus.usefulLinksMenu() == 0
 
 def test_usefulGeneralGroup(monkeypatch):
-
-    conn = sqlite3.connect('Accounts.db')
-    c = conn.cursor()
-    for r in c.execute("SELECT * FROM Accounts"):
-        print(r)
-
-    input = '2\n3\n4\n5\n6\n7\n1\njohndoe\np@ssw0rD\nJohn\nDoe\n0\n0\n0'
+    input = '2\n3\n4\n5\n6\n7\n0\n'
 
     monkeypatch.setattr('sys.stdin', io.StringIO(input))
 
     assert menus.usefulGeneralGroup() == 0
 
+
+# test for creating an account
+# When a user signs up, Email, SMS, and Targeted are all set 1 or True, Language is set to "English", and Stored in DB
+def test_createAccount(monkeypatch):
+
+    conn = sqlite3.connect('Accounts.db')
+    c = conn.cursor()
+    # display contents of db before signing up
+    for r in c.execute("SELECT * FROM Accounts"):
+        print(r)
+
+    input = 'johndoe\np@ssw0rD\nJohn\nDoe\n0\n0\n0'
+
+    monkeypatch.setattr('sys.stdin', io.StringIO(input))
+
+    assert menus.createAccountMenu() == 0
+
+    # display contents of db after signing up
     for r in c.execute("SELECT * FROM Accounts"):
         print(r)
