b0f9c87c0e1dfae1d2f51ac4b2f57d4b6eecdf3a
Zachary Chenoweth <90414605+zchenoweth@users.noreply.github.com>
2021-10-08 20:09:24 -0400

    Create test_ZacharyC.py

---
+++ InCollege Code/test_ZacharyC.py
@@ -0,0 +1,17 @@
+import sqlite3
+import pytest
+import io
+import personalProfile
+
+
+def test_create_profile_table():
+    return
+
+
+def test_update_profile():
+    return
+
+
+def test_formatCaps():
+    assert personalProfile("hEllO WoRLD!!!") == "Hello World!!!"
+    return
