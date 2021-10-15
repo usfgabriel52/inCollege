18acbf6771b08a4340eccfc3cb654516c77954ef
gc12301 <gabe.r.cortez@live.com>
2021-10-15 15:42:06 -0400

    Added comments

--- InCollege Code/friends.py
+++ InCollege Code/friends.py
@@ -4,6 +4,7 @@ from getpass import getpass
 conn = sqlite3.connect('InCollege.db')
 c = conn.cursor()
 
+#creates friends and request table if they dont exist
 def create_friends_tables():
     query = """CREATE TABLE IF NOT EXISTS friends(userName TEXT,friend TEXT)"""
     c.execute(query)
@@ -12,6 +13,8 @@ def create_friends_tables():
     c.execute(query)
     conn.commit()
 
+
+#Requests a friend
 def requestFriend(userName,requestee):
     if findSpecificFriend(userName, requestee).fetchall() != []:
         return 0
@@ -26,6 +29,9 @@ def requestFriend(userName,requestee):
     conn.commit()
     return 3
 
+
+
+#finds all friends of user, returns array of usernames
 def findFriends(userName):
     query = """SELECT * FROM friends WHERE userName = ?"""
     data = (userName)
@@ -40,7 +46,7 @@ def findFriends(userName):
         f.append(friend[0])
     return f
 
-
+#checks if a specific friend is in the table
 def findSpecificFriend(userName,friend):
     query = """SELECT * FROM friends WHERE userName = ? AND friend = ?"""
     data = (userName,friend)
@@ -52,39 +58,46 @@ def findSpecificFriend(userName,friend):
     else:
         return checkFriend
 
+#finds all friend requests for specific user
 def findRequests(userName):
     query = """SELECT * FROM requests WHERE requestee = ?"""
     data = (userName)
     return c.execute(query, [data])
 
+#Frinds a specific friend request 
 def findSpecificRequest(userName,requester):
     query = """SELECT * FROM requests WHERE userName = ? AND requestee = ?"""
     data = (requester,userName)
     return c.execute(query, data)
-    
+
+#PRIVATE METHOD: Deletes a request From requests table
 def deleteRequest(userName,requester):
     query = """DELETE FROM requests WHERE userName  = ? AND requestee = ?"""
     data = (requester,userName)
     c.execute(query,data)
     conn.commit()
 
+#PRIVATE METHOD, Adds friend to friends table 
 def addFriend(userName,friend):
     query = """INSERT INTO friends(userName, friend) VALUES(?,?)"""
     data = (userName,friend)
     c.execute(query,data)
     conn.commit()
+
+#Accepts friend request (removes request and adds friends to friends table)
 def acceptRequest(userName,requester):
     if findSpecificRequest(userName,requester).fetchall() == []:
         return 0
     deleteRequest(userName,requester)
     addFriend(userName, requester)
 
-
+#rejects a friend request
 def rejectReuest(userName,requester):
     if findSpecificRequest(userName,requester).fetchall() == []:
         return 0
     deleteRequest(userName,requester)
 
+#removes a friend
 def removeFriend(userName,friend):
     query = """DELETE FROM friends WHERE userName  = ? AND friend = ?"""
     data = (friend,userName)
