3898ae250c113e11139af2649e4c1c5d6ee54760
Zachary Chenoweth <90414605+zchenoweth@users.noreply.github.com>
2021-10-16 13:27:05 -0400

    Updated tests

--- InCollege Code/test_ZacharyC.py
+++ InCollege Code/test_ZacharyC.py
@@ -6,6 +6,36 @@ import menus
 import verify_acc
 import search
 
+
+#this function tests if you can create up to and no more than 10 accounts
+def test_maximum_num_accounts():
+    
+    conn = sqlite3.connect('InCollege.db')
+    c = conn.cursor()
+
+    #Creates a database for the profiles if one doesnt exisit
+    verify_acc.create_tables()
+    
+    #creates 10 user accounts
+    verify_acc.data_entry("zchenoweth", "Chenoweth1@", "Zachary", "Chenoweth", 1, 1, 1, "English")
+    verify_acc.data_entry("CoolDude123", "CoolDude1@", "John", "Smith", 1, 1, 1, "English")
+    verify_acc.data_entry("tester123", "Tester123@", "Anna", "Collins", 1, 1, 1, "English")
+    verify_acc.data_entry("Coder321", "Coder321@", "Henry", "Potter", 1, 1, 1, "English")
+    verify_acc.data_entry("princess123", "Princess1@", "Lucy", "Johnson", 1, 1, 1, "English")
+    verify_acc.data_entry("kingofpop1", "Hehe1234@", "Michael", "Jackson", 1, 1, 1, "English")
+    verify_acc.data_entry("metalhead123", "Metallica1@", "James", "Hetfield", 1, 1, 1, "English")
+    verify_acc.data_entry("Sweetypie", "Sweetpie1@", "Mary", "Noname", 1, 1, 1, "English")
+    verify_acc.data_entry("fakeperson", "Fake1234@", "Fake", "Person", 1, 1, 1, "English")
+    verify_acc.data_entry("Noideas123", "Noideas1@", "Larry", "Mason", 1, 1, 1, "English")
+
+    print("Test to add 11th account(should print all accounts are full):")
+    #should print that accounts are all full
+    assert menus.createAccountMenu() == 0
+
+    return
+
+
+
 #This function tests if a profile database is created and if a Student can create a profile and add it to the database
 def test_create_profile():
 
@@ -15,7 +45,7 @@ def test_create_profile():
     #create the database if it doesnt already exist
     verify_acc.create_tables()
     #creates a new profile and stores it in the database
-    personalProfile.create_profile("coolDude123")
+    personalProfile.create_profile("CoolDude123")
 
     # display contents of db before signing up
     for r in c.execute("SELECT * FROM personalProfile"):
@@ -40,37 +70,9 @@ def test_formatCaps():
 
 
 
-#this function tests if you can create up to and no more than 10 accounts
-def test_maximum_num_accounts():
-    
-    conn = sqlite3.connect('InCollege.db')
-    c = conn.cursor()
-
-    #Creates a database for the profiles if one doesnt exisit
-    verify_acc.create_tables()
-    
-    verify_acc.data_entry("zchenoweth", "Chenoweth1@", "Zachary", "Chenoweth", 1, 1, 1, "English")
-    verify_acc.data_entry("CoolDude123", "CoolDude1@", "John", "Smith", 1, 1, 1, "English")
-    verify_acc.data_entry("tester123", "Tester123@", "Anna", "Collins", 1, 1, 1, "English")
-    verify_acc.data_entry("Coder321", "Coder321@", "Henry", "Potter", 1, 1, 1, "English")
-    verify_acc.data_entry("princess123", "Princess1@", "Lucy", "Johnson", 1, 1, 1, "English")
-    verify_acc.data_entry("kingofpop1", "Hehe1234@", "Michael", "Jackson", 1, 1, 1, "English")
-    verify_acc.data_entry("metalhead123", "Metallica1@", "James", "Hetfield", 1, 1, 1, "English")
-    verify_acc.data_entry("Sweetypie", "Sweetpie1@", "Mary", "Noname", 1, 1, 1, "English")
-    verify_acc.data_entry("fakeperson", "Fake1234@", "Fake", "Person", 1, 1, 1, "English")
-    verify_acc.data_entry("Noideas123", "Noideas1@", "Larry", "Mason", 1, 1, 1, "English")
-
-    print("Test to add 11th account(should print all accounts are full):")
-    #should print that accounts are all full
-    assert menus.createAccountMenu() == 0
-
-    return
-
-
-
-
 def test_searchPeople():
     return
+
 
 test_maximum_num_accounts()    
 #test_create_profile()
