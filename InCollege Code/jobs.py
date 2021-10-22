fe213ce5f70354d64f09a4b3f72e27374573579b
Collin Chimbwanda <65328474+c05103@users.noreply.github.com>
2021-10-22 02:58:25 -0400

    Update jobs.py

--- InCollege Code/jobs.py
+++ InCollege Code/jobs.py
@@ -57,3 +57,60 @@ def postJob(posterfirst, posterlast):
     return 0
 
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#username shows job selected, or returns "0" to go back
+def list_jobs(username):
+    c = sqlite3.connect('InCollege.db')
+    conn = c.cursor()
+
+    jobs = conn.execute("SELECT * FROM jobs").fetchall()
+    store_list = []
+    count = 1
+    print("0. Back")
+    for i in jobs:
+        
+        #sends  title posted
+        store_list.append(i)
+        tmp = check_job_status(username, i[1], i[0])
+        print(str(count) + ". " +  "Title: " + str(i[1]) + "\t Employer: " + str(i[3]) + "\t Location: " + str(i[4]) + "\t Salary: " + str(i[5]) + "\t Description: " + str(i[2]) + "\t Status:"+ tmp)
+        count += 1
+    
+    c.close()
+
+    selection = job_selection(count-1)
+    if(selection == 0):
+        return 0
+
+    return store_list[selection-1]
+
+#/////////////
+
+def apply_job(job, current_user):
+    print("Apply for Jobs:")
+    c = sqlite3.connect('InCollege.db')
+    conn = c.cursor()    
+
+    #checks poster
+    if(str(job[0]) == str(current_user)):
+        print("Posted job, cannot apply.")
+        return
+
+    #checking if applied, if not it will 
+    status = str(check_job_status(current_user, job[1], job[0]))
+    if status == "applied":
+        print("Already applied")
+        return
+    elif status == "saved":
+        conn.execute("UPDATE app_status SET status = 'applied' WHERE username = '{}' AND title = '{}' AND status = 'saved'".format(current_user, job[1]))
+   
+    else:
+        conn.execute("INSERT INTO app_status VALUES ('{}', '{}', '{}', 'applied')".format(current_user, job[1], job[0]))
+
+    #inputs 
+    grad_date = input("Graduation date (mm/dd/yyyy)\n")
+    start_date = input("Start date you can begin work (mm/dd/yyyy)\n")
+    story = input("Why should you get this position.\n")
+    
+    #insert application to table
+    conn.execute("INSERT INTO applications VALUES ('{}', '{}', '{}', '{}', '{}', '{}')".format(current_user, job[1], job[3], grad_date, start_date, story))
+    c.commit()
